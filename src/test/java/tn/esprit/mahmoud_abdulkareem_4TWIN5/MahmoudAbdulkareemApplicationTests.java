274059223dadac55d781593d414149cae8a61e85
MahmoudAbdulkareem <mahmoud.abdulkareem@esprit.tn>
2024-11-11 21:48:09 +0100

    MJ
    Mise a Jour

--- src/test/java/tn/esprit/mahmoud_abdulkareem_4TWIN5/MahmoudAbdulkareemApplicationTests.java
+++ src/test/java/tn/esprit/mahmoud_abdulkareem_4TWIN5/MahmoudAbdulkareemApplicationTests.java
@@ -1,10 +1,10 @@
-package tn.esprit.ouday_oueslati_4TWIN5;
+package tn.esprit.mahmoud_abdulkareem_4TWIN5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OudayOueslatiApplicationTests {
+class MahmoudAbdulkareemApplicationTests {
 
     @Test
     void contextLoads() {
